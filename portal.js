717c793a3185d645bbef6c66e3a18a8eb6f5b09b
Mery De Freitas <67561739+MeryDeFreitas@users.noreply.github.com>
2022-05-26 21:05:32 -0300

    Add files via upload

--- portal.js
+++ portal.js
@@ -1,13 +1,11 @@
 let portal = document.getElementById('portal');
 
 for (let i = 0; i < 360; i++){
-    let chispa =
-    document.createElement("div");
+    let chispa = document.createElement("div");
     chispa.className = "chispa";
-    chispa.style.rotate = i * 2+ 'deg';
     let chispaTranslation = Math.random() * (120 - 100) + 100;
 
-    chispa.style.transform = 'translate('+chispaTranslation+'px)';
+    chispa.style.transform = 'rotate(' + i + 'deg) translate('+chispaTranslation+'px)';
 
     portal.appendChild(chispa);
-}
+    }
